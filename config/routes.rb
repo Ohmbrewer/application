d5ffd847fd5438a69eff8a58af83c1bd042387c9
Kyle Oliveira <kyle.oliveira@cornell.edu>
2015-04-06 00:44:40 -0400

    Added a Remember Me feature and fixed the login redirect to send the user to the homepage/main menu.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   get 'users/new'
 
-  root 'static_pages#home'
+  root 'sessions#new'
+  get  'home'    => 'static_pages#home'
   get  'help'    => 'static_pages#help'
   get  'about'   => 'static_pages#about'
   get  'contact' => 'static_pages#contact'
