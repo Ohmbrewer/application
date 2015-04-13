4f9ca5516d05c73672bb598cb471fd0645a9850f
Kyle Oliveira <kyle.oliveira@cornell.edu>
2015-04-12 22:20:26 -0400

    Changed the login redirection so that the user is sent to the home/controls page upon login.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
 
   def new
+    redirect_to home_url if logged_in?
   end
 
   def create
@@ -8,7 +9,7 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(params[:session][:password])
       log_in @user
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_back_or @user
+      redirect_back_or home_url
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
