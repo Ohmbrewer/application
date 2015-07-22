b00bfa1925292cd89a4f02334dd7bd70648904c3
Kyle Oliveira <kyle.oliveira@cornell.edu>
2015-07-22 01:21:04 -0400

    Swaps out the RubySpark gem in favor of the ParticleRb gem. Renames the Particle model to ParticleDevice so as to play nice with the new gem's modules (kinda makes sense semantically too, I guess). Cleans up the PingJob and adds rainbows to it.

--- app/models/rhizome.rb
+++ app/models/rhizome.rb
@@ -3,10 +3,10 @@ class Rhizome < ActiveRecord::Base
   # == Each Rhizome has a one-to-one relationship with a Particle. ==
   # This sets up that relationship and makes it so we can pass Particle attribute changes through the
   # Rhizome's controller rather than building a full, separate controller just for the associated Particle.
-  has_one :particle, inverse_of: :rhizome,
+  has_one :particle_device, inverse_of: :rhizome,
                      dependent: :destroy
 
-  accepts_nested_attributes_for :particle
+  accepts_nested_attributes_for :particle_device
 
   validates :name, presence: true,
                    length: { maximum: 50 }
